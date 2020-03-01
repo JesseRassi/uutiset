e0b8e12ec2812ad6591be483c415b3f8923229f5
lryop <37520731+lryop@users.noreply.github.com>
2020-03-01 08:11:55 +0200

    muokkaus

--- php/lisaa.php
+++ php/lisaa.php
@@ -5,13 +5,69 @@ $password = "";
 $dbname = "jobbaripojat";
 $yhteys = new mysqli($servername, $username, $password, $dbname);
 
-$id = haeID($yhteys) + 1;
+$muokkaus = $_GET["e"];
 $otsikko = $_POST["otsikko"];
 $kirjoittaja = $_POST["kirjoittaja"];
 $artikkeli = $_POST["artikkeli"];
-$avainsana = $_POST["avainsanat"];
+$avainsanat = str_word_count((string)$_POST["avainsanat"], 1);
 $kuvateksti = $_POST["kuvateksti"];
+$kuvapolku = $_POST["kuvapolku"];
 $pvm = date("Y-m-d H:i:s");
+
+if (!$muokkaus){
+    $id = hae_id($yhteys) + 1;
+} else {
+    $id = $_POST["id"];
+}
+
+if (!$muokkaus) {
+    #region // W3Schools image upload
+    $target_dir = "../img/";
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    // Check if image file is a actual image or fake image
+    if(isset($_POST["submit"])) {
+        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+        if($check !== false) {
+            echo "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+        } else {
+            echo "File is not an image.<br>";
+            $uploadOk = 0;
+        }
+    }
+    // Check if file already exists
+    if (file_exists($target_file)) {
+        echo "Sorry, file already exists.<br>";
+        $uploadOk = 0;
+    }
+    // Check file size
+    if ($_FILES["fileToUpload"]["size"] > 500000) {
+        echo "Sorry, your file is too large.<br>";
+        $uploadOk = 0;
+    }
+    // Allow certain file formats
+    if($imageFileType != "jpg") {
+        echo "Sorry, only JPG files are allowed.<br>";
+        $uploadOk = 0;
+    }
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.<br>";
+    // if everything is ok, try to upload file
+    } else {
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }
+    #endregion
+} else {
+    $kuvapolku = $_POST["kuvapolku"];
+}
+
 $check_uutiset = (int)isset($_POST["uutiset"]);
 $check_kotimaa = (int)isset($_POST["kotimaa"]);
 $check_ulkomaat = (int)isset($_POST["ulkomaat"]);
@@ -21,77 +77,45 @@ $check_urheilu = (int)isset($_POST["urheilu"]);
 $check_viihde = (int)isset($_POST["viihde"]);
 $check_terveys = (int)isset($_POST["terveys"]);
 
-$avainsanat = str_word_count($avainsana, 1);
 $as0 = $avainsanat[0];
 $as1 = $avainsanat[1];
 $as2 = $avainsanat[2];
 $as3 = $avainsanat[3];
 $as4 = $avainsanat[4];
 
-$sql = "INSERT INTO uutiset (id, xml, pvm, avainsana0, avainsana1, avainsana2, avainsana3, avainsana4, uutiset, kotimaa, ulkomaat, politiikka, talous, urheilu, viihde, terveys) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+if(!$muokkaus){
+    $sql = "INSERT INTO uutiset (pvm, avainsana0, avainsana1, avainsana2, avainsana3, avainsana4, uutiset, kotimaa, ulkomaat, politiikka, talous, urheilu, viihde, terveys, id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+} else {
+    $sql = "UPDATE uutiset SET pvm = ?, avainsana0 = ?, avainsana1 = ?, avainsana2 = ?, avainsana3 = ?, avainsana4 = ?, uutiset = ?, kotimaa = ?, ulkomaat = ?, politiikka = ?, talous = ?, urheilu = ?, viihde = ?, terveys = ? WHERE id = ?";
+}
 $stmt = mysqli_prepare($yhteys, $sql);
-mysqli_stmt_bind_param($stmt, "iissssssiiiiiiii", $id, $id, $pvm, $as0, $as1, $as2, $as3, $as4, $check_uutiset, $check_kotimaa, $check_ulkomaat, $check_politiikka, $check_talous, $check_urheilu, $check_viihde, $check_terveys);
+mysqli_stmt_bind_param($stmt, "ssssssiiiiiiiii", $pvm, $as0, $as1, $as2, $as3, $as4, $check_uutiset, $check_kotimaa, $check_ulkomaat, $check_politiikka, $check_talous, $check_urheilu, $check_viihde, $check_terveys, $id);
 mysqli_stmt_execute($stmt);
 mysqli_stmt_close($stmt);
 mysqli_close($yhteys);
 
-#region // W3Schools
-$target_dir = "../img/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.<br>";
-        $uploadOk = 0;
-    }
-}
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.<br>";
-    $uploadOk = 0;
-}
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.<br>";
-    $uploadOk = 0;
-}
-// Allow certain file formats
-if($imageFileType != "jpg") {
-    echo "Sorry, only JPG files are allowed.<br>";
-    $uploadOk = 0;
-}
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.<br>";
-// if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
-}
-#endregion
-
 $uutinen = new DOMDocument("1.0", "utf-8");
 $uutinen->formatOutput = true;
-$uutinen_wrap = $uutinen->createElement("Uutinen");
-$uutinen_otsikko = $uutinen->createElement("Otsikko", $otsikko);
-$uutinen_kirjoittaja = $uutinen->createElement("Kirjoittaja", $kirjoittaja);
-$uutinen_artikkeli = $uutinen->createElement("Artikkeli", $artikkeli);
-$uutinen_kuva = $uutinen->createElement("Kuvapolku", $target_file);
-$uutinen_kuvateksti = $uutinen->createElement("Kuvateksti", $kuvateksti);
+// luodaan xml elementit
+$uutinen_wrap = $uutinen->createElement("uutinen");
+$uutinen_id = $uutinen->createElement("id", $id);
+$uutinen_otsikko = $uutinen->createElement("otsikko", $otsikko);
+$uutinen_kirjoittaja = $uutinen->createElement("kirjoittaja", $kirjoittaja);
+$uutinen_artikkeli = $uutinen->createElement("artikkeli", $artikkeli);
+$uutinen_kuvateksti = $uutinen->createElement("kuvateksti", $kuvateksti);
+// jos muokataan artikkelia, ei päivitetä kuvapolkua
+if(!$muokkaus){
+    $uutinen_kuva = $uutinen->createElement("kuvapolku", $target_file);
+} else {
+    $uutinen_kuva = $uutinen->createElement("kuvapolku", $kuvapolku);
+}
+// siirretään xml elementit uutinen -wrapperin sisään
+$uutinen_wrap->appendChild($uutinen_id);
 $uutinen_wrap->appendChild($uutinen_otsikko);
 $uutinen_wrap->appendChild($uutinen_kirjoittaja);
 $uutinen_wrap->appendChild($uutinen_artikkeli);
-$uutinen_wrap->appendChild($uutinen_kuva);
 $uutinen_wrap->appendChild($uutinen_kuvateksti);
+$uutinen_wrap->appendChild($uutinen_kuva);
 $uutinen->appendChild($uutinen_wrap);
 $uutinen->save("../xml/{$id}.xml");
 
@@ -122,7 +146,7 @@ function get_words($sentence, $count) {
     return $matches[0];
 }
 
-function haeID($yhteys){
+function hae_id($yhteys){
     $sql = "SELECT MAX(id) FROM uutiset LIMIT 1";
     $stmt = mysqli_prepare($yhteys, $sql);
     mysqli_stmt_execute($stmt);
---
+++ php/muokkaa.php
@@ -0,0 +1,83 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "jobbaripojat";
+$yhteys = new mysqli($servername, $username, $password, $dbname);
+
+$id = 7;
+
+$sql = "SELECT * FROM uutiset WHERE id = ?";
+$stmt = mysqli_prepare($yhteys, $sql);
+mysqli_stmt_bind_param($stmt, "i", $id);
+mysqli_stmt_execute($stmt);
+$row = mysqli_fetch_object(mysqli_stmt_get_result($stmt));
+mysqli_stmt_close($stmt);
+
+$avainsanat = $row->avainsana0 . " " . $row->avainsana1 . " " . $row->avainsana2 . " " . $row->avainsana3 . " " . $row->avainsana4;
+$check_uutiset = $row->uutiset;
+$check_kotimaa = $row->kotimaa;
+$check_ulkomaat = $row->ulkomaat;
+$check_politiikka = $row->politiikka;
+$check_talous = $row->talous;
+$check_urheilu = $row->urheilu;
+$check_viihde = $row->viihde;
+$check_terveys = $row->terveys;
+
+$file = "../xml/{$id}.xml";
+$xml = simplexml_load_file($file);
+?>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Muokkaa Artikkelia</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/tyyli.css">
+</head>
+
+<body style="height:100vh">
+
+    <div class="container-fluid h-100">
+        <div class="row h-100">
+            <div class="col mx-auto"></div>
+            <div class="col-md-6 bg-primary h-100">
+                <form action="lisaa.php?e=true" method="POST" enctype="multipart/form-data">
+                    <input type="number" name="id" value="<?php echo $xml->id;?>" readonly style="display: none">
+                    <br>Otsikko:<br>
+                    <input type="text" name="otsikko" value="<?php echo $xml->otsikko;?>">
+                    <br>Kirjoittaja:<br>
+                    <input type="text" name="kirjoittaja" value="<?php echo $xml->kirjoittaja;?>">
+                    <br>Artikkeli:<br>
+                    <textarea name="artikkeli"><?php echo $xml->artikkeli;?></textarea>
+                    <br>Valitse aiheet:<br>
+                    <ul>
+                        <li><input type="checkbox" name="uutiset" <?php echo ($check_uutiset) ? "checked" : false;?>> Uutiset</li>
+                        <li><input type="checkbox" name="kotimaa" <?php echo ($check_kotimaa) ? "checked" : false;?>> Kotimaa</li>
+                        <li><input type="checkbox" name="ulkomaat" <?php echo ($check_ulkomaat) ? "checked" : false;?>> Ulkomaat</li>
+                        <li><input type="checkbox" name="politiikka" <?php echo ($check_politiikka) ? "checked" : false;?>> Politiikka</li>
+                        <li><input type="checkbox" name="talous" <?php echo ($check_talous) ? "checked" : false;?>> Talous</li>
+                        <li><input type="checkbox" name="urheilu" <?php echo ($check_urheilu) ? "checked" : false;?>> Urheilu</li>
+                        <li><input type="checkbox" name="viihde" <?php echo ($check_viihde) ? "checked" : false;?>> Viihde</li>
+                        <li><input type="checkbox" name="terveys" <?php echo ($check_terveys) ? "checked" : false;?>> Terveys</li>
+                    </ul>
+                    Avainsanat (5):<br>
+                    <input type="text" name="avainsanat" value="<?php echo $avainsanat;?>">
+                    <br>Kuvateksti:<br>
+                    <input type="text" name="kuvateksti" value="<?php echo $xml->kuvateksti;?>">
+                    <input type="text" name="kuvapolku" value="<?php echo $xml->kuvapolku;?>" readonly style="display: none">
+                    <br><br>
+                    <input type="submit" name="laheta" value="Lähetä">
+                </form>
+            </div>
+            <div class="col mx-auto"></div>
+        </div>
+    </div>
+
+    <script src="js/jquery-3.4.1.slim.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
