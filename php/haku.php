b78245f176aab5492dde19d3e3cc5a1f1f5f1bd1
AnttiSyrjanen <anttisyr@gmail.com>
2020-03-03 12:43:29 +0200

    Create haku.php

---
+++ php/haku.php
@@ -0,0 +1,127 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "jobbaripojat";
+$yhteys = new mysqli($servername, $username, $password, $dbname);
+$hakusana- = $_GET["hakusana"];
+
+function hae_uutinen($id){
+    $file = "xml/{$id}.xml";
+    $xml = simplexml_load_file($file);
+    return $xml;
+}
+function get_words($sentence, $count = 15) {
+    preg_match("/(?:[^\s,\.;\?\!]+(?:[\s,\.;\?\!]+|$)){0,$count}/", $sentence, $matches);
+    return $matches[0];
+}
+
+function uutiset($yhteys, $hakusana){
+
+    $sql = "SELECT id, pvm FROM uutiset WHERE avainsana0, avainsana1, avainsana2, avainsana3, avainsana4 = {$hakusana} ORDER BY pvm DESC LIMIT 5";
+    
+    $result = $yhteys->query($sql);
+    for ($set = array (); $row = $result->fetch_assoc(); $set[] = $row);
+    $i = (int)0;
+    while ($i < count($set)) {
+        $id = $set[$i]["id"];
+        echo 
+        '<div class="media" id="' . hae_uutinen($id)->id . '" onclick="location.href=' . "'php/uutinen.php?id=" . hae_uutinen($id)->id . "'" . '">
+        <img class="mr-3" src="' . hae_uutinen($id)->kuvapolku . '" height="300px" width="450px">
+        <div class="media-body">
+            <span>Posted at ' . $set[$i]['pvm'] . '</span>
+            <h5 class="mt-0">' . hae_uutinen($id)->otsikko . '</h5>
+            <p>' . substr(get_words(hae_uutinen($id)->artikkeli), 0, -2) . '..</p>
+        </div>
+        </div>
+        <div class="row mt-3">
+        </div>'
+        ;
+        $i++;
+    }
+}
+?>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Uutiset</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/tyyli.css">
+</head>
+
+<body style="height:100vh">
+
+    <div class="container-fluid h-100">
+
+        <div class="row h-100">
+
+            <div class="col mx-auto">
+                <!-- A vertical navbar -->
+                <nav class="navbar bg-light float-right">
+
+                    <!-- Links -->
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Koti</a>
+                        </li>
+                        <div class="dropdown">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Aiheet
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <a class="dropdown-item" href="#">Politiikka</a>
+                                <a class="dropdown-item" href="#">Urheilu</a>
+                                <a class="dropdown-item" href="#">Kotimaa</a>
+                                <a class="dropdown-item" href="#">Ulkomaa</a>
+
+                            </div>
+                            <div class="row mt-3">
+
+                            </div>
+
+                            <div class="md-form mt-0">
+
+                                <input class="form-control" type="text" placeholder="Haku" aria-label="Haku">
+
+                            </div>
+
+                        </div>
+                    </ul>
+                </nav>
+            </div>
+            <div class="col-md-6 h-100">
+
+
+
+                <!-- <div class="media" id="<?php //echo hae_uutinen(2)->id;?>" onclick="location.href='php/uutinen.php?id=' + this.id">
+                    <img class="mr-3" src="<?php //echo hae_uutinen(2)->kuvapolku?>" height="300px" width="450px">
+                    <div class="media-body">
+                        <h5 class="mt-0"><?php //echo hae_uutinen(2)->otsikko?></h5>
+                        <p><?php //echo get_words(hae_uutinen(2)->artikkeli);?></p>
+                    </div>
+                </div>
+
+                <div class="row mt-3">
+
+                </div>
+
+                <div class="media">
+                    <img class="mr-3" src="https://files.luolasto.org/20585/tiedosto.jpg" height="300px" width="450px">
+                    <div class="media-body">
+                        <h5 class="mt-0">Uutisotsikko</h5> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    </div>
+                </div> -->
+
+            </div>
+            <div class="col mx-auto"></div>
+        </div>
+    </div>
+
+    <script src="js/jquery-3.4.1.slim.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
