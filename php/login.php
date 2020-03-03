00adaac5deb7904c9a028ccbc751f4da74e93aea
JesseRassi <59819027+JesseRassi@users.noreply.github.com>
2020-03-03 15:49:11 +0200

    Add files via upload

---
+++ php/login.php
@@ -0,0 +1,21 @@
+<?php
+// Always start this first
+session_start();
+
+if ( ! empty( $_POST ) ) {
+    if ( isset( $_POST['username'] ) && isset( $_POST['password'] ) ) {
+        // Getting submitted user data from database
+        $yhteys = new mysqli($servername, $username, $password, $dbname);
+        $stmt = $yhteys->prepare("SELECT * FROM yllapito WHERE kayttaja = ?");
+        $stmt->bind_param('s', $_POST['username']);
+        $stmt->execute();
+        $result = $stmt->get_result();
+    	$user = $result->fetch_object();
+    		
+    	// Verify user password and set $_SESSION
+    	if ( password_verify( $_POST['password'], $user->password ) ) {
+    		$_SESSION['user_id'] = $user->ID;
+    	}
+    }
+}
+?>
